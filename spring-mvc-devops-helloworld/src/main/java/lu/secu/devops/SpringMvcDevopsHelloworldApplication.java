d947d254b87a923fdf1cfed4023aaebe7218c14a
Benjamin JAHIC <>
2023-02-23 20:27:29 +0100

    changes project

---
+++ spring-mvc-devops-helloworld/src/main/java/lu/secu/devops/SpringMvcDevopsHelloworldApplication.java
@@ -0,0 +1,13 @@
+package lu.secu.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcDevopsHelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcDevopsHelloworldApplication.class, args);
+	}
+
+}
---
+++ spring-mvc-devops-helloworld/src/test/java/lu/secu/devops/SpringMvcDevopsHelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package lu.secu.devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcDevopsHelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
