3336a7db0585217cd9abbc0702830949d860b96f
Benjamin JAHIC <>
2023-12-09 17:56:51 +0100

    My Commit message ...

--- spring-mvc-devops-helloworld/src/test/java/lu/secu/devops/AffiliationsBrokerTest.java
+++ spring-mvc-devops-helloworld/src/test/java/lu/secu/devops/AffiliationsBrokerTest.java
@@ -2,12 +2,11 @@ package lu.secu.devops;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.assertj.core.api.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class HomeControllerTest {
+public class AffiliationsBrokerTest {
 
 	@Test
 	void loadingHomeController() {
